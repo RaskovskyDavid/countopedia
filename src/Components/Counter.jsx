f0e5822f608875f2898c0656d6deb93ed060bcc7
david <davidtucu@hotmail.com>
2023-12-04 08:59:54 -0300

    botones de contadores

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -3,7 +3,10 @@ export default class Counter extends React.Component {
     render()
     {
         return(
-            <div>
+            <div className="row text-white">
+                <h1>Counter: </h1>
+                <button style={{ width: "200px" }}>+1</button>
+                <button style={{ width: "200px" }}>-1</button>
             </div>
         )
 
